19e7a75a954457acea96321403865c1cc0423264
wolvesdevs_Work <75551104+wolvesdevs@users.noreply.github.com>
2023-11-09 18:43:11 +0900

    03_手続き型で実装_描画

---
+++ src/DesignPattern1/A030/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace A030
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.AButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 378);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(12, 396);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(169, 42);
+            this.RightButton.TabIndex = 1;
+            this.RightButton.Text = "→";
+            this.RightButton.UseVisualStyleBackColor = true;
+            // 
+            // AButton
+            // 
+            this.AButton.Location = new System.Drawing.Point(201, 396);
+            this.AButton.Name = "AButton";
+            this.AButton.Size = new System.Drawing.Size(169, 42);
+            this.AButton.TabIndex = 2;
+            this.AButton.Text = "A";
+            this.AButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button AButton;
+    }
+}
+
---
+++ src/DesignPattern1/A030/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A030
+{
+    public partial class Form1 : Form
+    {
+        private int _marioX = 10;
+        private int _marioY = 10;
+
+        private int _luigiX = 10;
+        private int _luigiY = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.Red), _marioX, _marioY, 30, 30);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Lime), _luigiX, _luigiY, 30, 30);
+        }
+    }
+}
